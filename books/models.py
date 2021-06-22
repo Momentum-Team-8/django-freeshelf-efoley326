77c291d8bae6a975ff4c81de45a16f4456e97c21
Emily Foley <efoley326@gmail.com>
2021-06-21 22:02:52 -0400

    goal one

--- books/models.py
+++ books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from django.utils import timezone
 
 class User(AbstractUser):
     def __repr__(self):
@@ -8,3 +8,13 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+
+
+class book_info(models.Model):
+    title = models.CharField(max_length=100, help_text='Book Title')
+    author = models.CharField(max_length=100, help_text='Book Author')
+    description = models.CharField(max_length=1000, help_text='Book Description')
+    book_link = models.CharField(max_length= 100, help_text='Book Link')
+    created_at = models.DateTimeField(max_length=20, help_text='book added at:')
